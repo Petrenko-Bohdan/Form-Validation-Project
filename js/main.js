b1d48e477c74d029372a06831d94c9bf2f719e62
Bohdan Petrenko <bohdan.petrenko.web@gmail.com>
2023-12-03 16:51:09 +0100

    add all files to repository

---
+++ js/main.js
@@ -0,0 +1,40 @@
+import Validator from "./validator.js";
+
+const validator = new Validator();
+
+function showErr(field, errText){
+	const err = document.createElement('span');
+	field.after(err);
+	err.textContent = errText;
+}
+
+form.addEventListener('submit', event =>{
+	event.preventDefault();	
+
+	const emailValid = validator.isEmail(formEmail.value);
+	const passwordValid = validator.isPassword(formPassword.value);
+	const confirmPasswordValid = validator.isPassword(formConfirmPassword.value);
+	const dateValid = validator.isDate(formDate.value);
+
+	let errorMessage;
+
+	if(!emailValid){
+		 errorMessage = 'Invalid email. ';
+		 showErr(formEmail, errorMessage);
+	}
+
+	if(!passwordValid){
+		errorMessage = 'Invalid password.';
+		showErr(formPassword, errorMessage);
+	}
+
+	if(!confirmPasswordValid){
+		errorMessage = 'Invalid confirm password.';
+		showErr(formConfirmPassword, errorMessage);
+	}
+
+	if(!dateValid){
+		errorMessage = 'Invalid date.';
+		showErr(formDate, errorMessage);
+	}
+})
